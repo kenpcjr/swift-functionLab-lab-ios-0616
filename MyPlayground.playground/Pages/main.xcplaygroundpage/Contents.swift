dc42553da9c12c0629c38569ac324c3895241b2d
kenpcjr <kennethcooke@me.com>
2016-07-05 14:47:58 -0400

    Done.

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,11 @@
  */
 // write your code here
 
+func helloWorld(){
+    print("Hello, World!")
+}
 
+helloWorld()
 
 
 /*: question2
@@ -21,7 +25,12 @@
  */
 // write your code here
 
+func constantPrint(){
+    let thisConstant = "Yep"
+    print(thisConstant)
+}
 
+constantPrint()
 
 
 /*: question3
@@ -29,7 +38,13 @@
  */
 // write your code here
 
+func greetHuman(name : String){
+    print("Howdy \(name)!")
+}
 
+greetHuman("Ken")
+greetHuman("Bob")
+greetHuman("Betty")
 
 
 /*: question4
@@ -37,13 +52,32 @@
  */
 // write your code here
 
+var humanName = "Rich Pound"
 
+greetHuman(humanName)
+
+humanName = "Mr. Pound"
+
+greetHuman(humanName)
 
 
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
+
+func printVariable(){
+    var thisVariable = "ME"
+    print(thisVariable)
+    thisVariable = "YOU"
+    print(thisVariable)
+}
+
+printVariable()
+
+printVariable()
+
+
 
 
 
